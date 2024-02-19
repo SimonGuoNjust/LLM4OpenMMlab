893851acc3d94512c0b12031336de4c607180774
Xujing Guo <guoxujing@njust.edu.cn>
2024-02-19 13:21:44 +0800

    construct

--- start.py
+++ start.py
@@ -1,3 +1,18 @@
 import os
 
+open_mmlab_repos = [
+    "mmcv",
+    "mmyolo",
+    "mmdetection",
+    "mmdetection3d",
+    "mmengine",
+    "mmocr",
+    "mmsegmentation",
+    "mmhuman3d",
+    "mmrotate",
+    "mmediting",
+]
+for repo in open_mmlab_repos:
+    os.system(f"git clone https://gitee.com/open-mmlab/{repo}.git")
+os.system('python construct.py')
 os.system('streamlit run app.py --server.address=0.0.0.0 --server.port 7860')
